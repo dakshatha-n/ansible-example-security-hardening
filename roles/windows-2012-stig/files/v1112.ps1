c6da18ca71b149ccfb36facc2902b7ca3917edf3
Tyler Cross <tyler@cross.solutions>
2019-04-11 17:07:29 -0400

    add extra windows modules

--- roles/windows-2012-stig/files/v1112.ps1
+++
@@ -1,9 +0,0 @@
-([ADSI]('WinNT://{0}' -f $env:COMPUTERNAME)).Children | Where { $_.SchemaClassName -eq 'user' } | ForEach { 
-$user = ([ADSI]$_.Path) 
-$lastLogin = $user.Properties.LastLogin.Value 
-$enabled = ($user.Properties.UserFlags.Value -band 0x2) -ne 0x2 
-if ($lastLogin -eq $null) { 
-$lastLogin = 'Never' 
-} 
-Write-Host $user.Name $lastLogin $enabled 
-} 
